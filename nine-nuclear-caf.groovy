338ba8783cee89c773f532482d6ba927cdfacb7f
Anay Wadhera <anay1018@gmail.com>
2019-08-06 21:57:10 -0700

    Update nine-nuclear-caf.groovy

--- nine-nuclear-caf.groovy
+++ nine-nuclear-caf.groovy
@@ -1,19 +1,20 @@
 # takes parameters: device, buildtype, statix_build_type, and repopick
-def BUILD_TREE = "/home/buildbot/stx-caf"
 node {
-      stage('Sync') {
-        telegramSend 'Started build of $DEVICE $BUILDTYPE';
-        telegramSend 'Job link: $BUILD_URL';
+    currentBuild.displayName = "$DEVICE"
+    currentBuild.description = "Build ID: $BUILD_NUMBER"
+    def BUILD_TREE="$BUILD_HOME/stx-caf"
+    stage('Sync') {
+        telegramSend("Syncing source...")
     	sh '''#!/bin/bash
 		cd '''+BUILD_TREE+'''
 		. venv/bin/activate
 		repo init --depth=1 -u https://github.com/StatiXOS/android_manifest.git -b 9-caf --no-tags
 		rm -rf .repo/local_manifests
 	    repo sync -d -c --force-sync --no-tags --no-clone-bundle -j8
+	    repo forall -vc "git reset --hard"
 		'''
-		telegramSend 'Sync completed.';
-  }
-  stage('Clean') {
+    }
+    stage('Clean') {
 		sh '''#!/bin/bash
 		cd '''+BUILD_TREE+'''
 		make clean
@@ -21,6 +22,8 @@ node {
 		'''
   }
   stage('Build') {
+      telegramSend("Starting build of $DEVICE")
+      telegramSend("Job url: $BUILD_URL")
 		sh '''#!/bin/bash +e
 		cd '''+BUILD_TREE+'''
 		. venv/bin/activate
@@ -33,17 +36,14 @@ node {
 		if [[ ! -z "${REPOPICK}" ]]; then repopick -f ${REPOPICK}; else echo "No Commits to pick!"; fi
 		mka bacon
 		'''
-		telegramSend 'Build completed,  uploading.';
   }
   stage('Upload') {
+      telegramSend("Uploading build of $DEVICE")
       	sh '''#!/bin/bash
       	set -e
 		echo "Deploying artifacts..."
-		rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" $OUT_DIR_COMMON_BASE/stx-caf/target/product/$DEVICE/ anayw2001@storage.osdn.net:/storage/groups/s/st/statixos/$DEVICE/9/
+		rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" $OUT_DIR_COMMON_BASE/stx-caf/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/$DEVICE/nuclear/
 		'''
-		telegramSend 'Build uploaded!';
-		sh '''#!/bin/bash
-		rm -rf out
-		'''
+        telegramSend("Upload complete!")
   }
 }
